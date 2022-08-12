18ae1fd205424380fc3f51dd02bbca98daaf80d6
Yashwant <yashwantsahu3002@gmail.com>
2022-08-12 10:08:56 +0530

    /**

---
+++ Medium/Q4.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ 
+ Container With Most Water
+ https://leetcode.com/problems/container-with-most-water/
+ */
+var maxArea = function(height) {
+    
+    let max = 0;
+    let left = 0;
+    let right = (height.length)-1;
+    
+    while(1)
+        {
+            
+            let check  = Math.abs(left-right) * Math.min(height[left],height[right])
+            
+            if (max<check) max = check;
+            
+            if(height[left] >= height[right])
+                    right--;
+            else if(height[left] < height[right])
+                    left++;
+            
+            if (left === right) break;
+        }
+    
+    return max;
+    
+};
